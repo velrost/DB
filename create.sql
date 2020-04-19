cab95e2fdea5131a3ac943650aa926e24e4fbe40
velrost <46864175+velrost@users.noreply.github.com>
2020-04-19 03:23:01 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,22 +1,20 @@
-REM   Script: create
-REM   creating tables
-
 create table winner( 
-      winner_name varchar(50) not null PRIMARY KEY,
+      winner_name varchar(50) not null PRIMARY KEY, 
       total_wins integer DEFAULT NULL
 );
 
 create table top_scorer( 
       top_scorer_name varchar(50) not null PRIMARY KEY, 
       club VARCHAR(50) NOT NULL, 
-      goals integer DEFAULT NULL 
+      total_goals integer DEFAULT NULL
 );
 
 create table season( 
       season_index integer DEFAULT NULL PRIMARY KEY, 
       season_year integer DEFAULT NULL, 
       winner_name VARCHAR(50) NOT NULL REFERENCES winner(winner_name), 
-      top_scorer_name VARCHAR(50) NOT NULL REFERENCES top_scorer(top_scorer_name) 
+      top_scorer_name VARCHAR(50) NOT NULL REFERENCES top_scorer(top_scorer_name),
+      goals integer DEFAULT NULL
 );
 
 
