6710a91638ae0d2ecf3607b9eed93f6b91c51763
velrost <46864175+velrost@users.noreply.github.com>
2020-04-19 03:25:19 +0300

    Add files via upload

---
+++ populate.sql
@@ -0,0 +1,74 @@
+INSERT INTO WINNER ( winner_name, total_wins)
+VALUES ( 'Barcelona', '1');
+
+INSERT INTO WINNER ( winner_name, total_wins)
+VALUES ( 'Athletic Bilbao', '4');
+
+INSERT INTO WINNER ( winner_name, total_wins)
+VALUES ( 'Real Madrid', '2');
+
+INSERT INTO WINNER ( winner_name, total_wins)
+VALUES ( 'Real Betis', '1');
+
+INSERT INTO WINNER ( winner_name, total_wins)
+VALUES ( 'Atletico Aviacion [A]', '1');
+
+INSERT INTO WINNER ( winner_name, total_wins)
+VALUES ( 'Atletico Aviacion', '1');
+
+
+INSERT INTO TOP_SCORER (top_scorer_name, club, total_goals)
+VALUES ( 'Paco Bienzobas', 'Real Sociedad', '14' );
+
+INSERT INTO TOP_SCORER (top_scorer_name, club, total_goals)
+VALUES ( 'Guillermo Gorostiza', 'Athletic Bilbao', '31' );
+
+INSERT INTO TOP_SCORER (top_scorer_name, club, total_goals)
+VALUES ( 'Agustin Sauto Arana', 'Athletic Bilbao', '27' );
+
+INSERT INTO TOP_SCORER (top_scorer_name, club, total_goals)
+VALUES ( 'Manuel Olivares', 'Real Madrid', '16' );
+
+INSERT INTO TOP_SCORER (top_scorer_name, club, total_goals)
+VALUES ( 'Isidro Langara', 'Real Oviedo', '80' );
+
+INSERT INTO TOP_SCORER (top_scorer_name, club, total_goals)
+VALUES ( 'Victor Unamuno', 'Athletic Bilbao', '22' );
+
+INSERT INTO TOP_SCORER (top_scorer_name, club, total_goals)
+VALUES ( 'Pruden', 'Atletico Aviacion', '30' );
+
+
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('1', '1929', 'Barcelona', 'Paco Bienzobas', '14');
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('2', '1930', 'Athletic Bilbao', 'Guillermo Gorostiza', '19');
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('3', '1931', 'Athletic Bilbao', 'Agustin Sauto Arana', '27');
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('4', '1932', 'Real Madrid', 'Guillermo Gorostiza', '12');
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('5', '1933', 'Real Madrid', 'Manuel Olivares', '16');
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('6', '1934', 'Athletic Bilbao', 'Isidro Langara', '27');
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('7', '1935', 'Real Betis', 'Isidro Langara', '26');
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('8', '1936', 'Athletic Bilbao', 'Isidro Langara', '27');
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('9', '1940', 'Atletico Aviacion [A]', 'Victor Unamuno', '22');
+
+INSERT INTO season (season_index, season_year, winner_name, top_scorer_name, goals)
+VALUES ('10', '1941', 'Atletico Aviacion', 'Pruden', '30');
+
+
+
